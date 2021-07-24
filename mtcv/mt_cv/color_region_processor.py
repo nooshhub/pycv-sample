29b602a85ef7687e389dde91a956c456d4a482c1
neals <shandeai520@gmail.com>
2021-07-24 16:22:32 +0800

    color region processor refactor to use pre-saved color to filter out color on image

--- mtcv/mt_cv/color_region_processor.py
+++ mtcv/mt_cv/color_region_processor.py
@@ -1,61 +1,48 @@
 import cv2 as cv
+import numpy as np
+
+from mt_cv import test_util, image_util
+from mt_cv.color_data import bgr_colors
 
 
-def find_bgr_colors(src, cnts):
-    """找出颜色示例里的颜色BGR
+def process(img_path, debug=False):
+    """从color_data.bgr_colors里过滤出当前图片中使用到的颜色，减少色块匹配的循环次数"""
 
-    根据限定长，高，比例来过滤出实例颜色区域
-    色块宽度是输入图片宽度的0.3到0.4之间
-    色块宽高的比例是0.4到0.5之间
-
-    Args:
-        src: 输入图像
-        cnts: 轮廓
-
-    Returns:
-        bgr colors
-    """
-    w_range = [0.3 * src.shape[1], 0.4 * src.shape[1]]
-    ratio_range = [0.40, 0.50]
-    colors = []
-
-    for cnt in cnts:
-        x, y, w, h = cv.boundingRect(cnt)
-        ratio = round(h / w, 2)
-
-        if ratio_range[0] < ratio < ratio_range[1] and w_range[0] < w < w_range[1]:
-            # print(ratio, x, y, w, h)
-
-            # 因为，原图色块矩形的周边和mask出来的颜色区都有模糊渐变的线
-            # 无法使用cv.mean(colorRegion, meanMask)来计算实际颜色
-            # 所以，取矩形的中心点的颜色最为准确
-            cx, cy = round(x + w / 2), round(y + h / 2)
-            bgr = src[cy, cx]
-            colors.append(bgr.tolist())
-
-    return colors
-
-
-def process(img_path):
     src = cv.imread(img_path)
-    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
 
-    # 低于thresh都变为黑色，maxval是给binary用的
-    threshold = cv.threshold(gray, 254, 255, cv.THRESH_BINARY_INV)[1]
+    filtered_bgr_colors = []
 
-    contours, hierarchy = cv.findContours(threshold, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    for bgr in bgr_colors:
+        # 图片里的颜色可能和示例颜色不相等，适当增加点阈值来防色差
+        threshold = 5
+        lower = np.array(image_util.bgr_with_threshold(bgr, -threshold), dtype="uint8")
+        upper = np.array(image_util.bgr_with_threshold(bgr, threshold), dtype="uint8")
+        # 根据阈值找到对应颜色区域，黑底白块
+        mask = cv.inRange(src, lower, upper)
 
-    bgr_colros = find_bgr_colors(src, contours)
-    # print(bgr_colros)
-    return bgr_colros
+        # 过滤出于颜色匹配的色块
+        non_zero_count = cv.countNonZero(mask)
+        if non_zero_count == 0:
+            continue
+
+        if debug:
+            # 黑白变白黑
+            mask_inv = 255 - mask
+            # 展示图片
+            output = cv.bitwise_and(src, src, mask=mask_inv)
+            test_util.show_compare_img(src, output)
+
+        filtered_bgr_colors.append(bgr)
+    return filtered_bgr_colors
 
 
 if __name__ == '__main__':
-    id = 'id2'
-    file_name = 'color_region.png'
-    img_path = '../images/' + id + '/' + file_name
+    image_folder = '../images/tmp/0a0937ba-ca3e-4d8c-9c3b-dbecfb5b1d68'
+    img_path = image_folder + '/color_region.png'
 
-    process(img_path)
+    # bgr_colros = process(img_path, debug=True)
+    bgr_colros = process(img_path)
+    print(bgr_colros)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
